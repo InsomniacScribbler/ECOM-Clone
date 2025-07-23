be73f2ba9f7f9027310ba944f16068b35f878dac
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-07-23 15:14:04 +0530

    Intialized StudentRepository

--- src/main/java/com/nikhilzzz/ecommerce/SB_Ecom/Repository/CategoryRepo.java
+++ src/main/java/com/nikhilzzz/ecommerce/SB_Ecom/Repository/CategoryRepo.java
@@ -3,5 +3,5 @@ package com.nikhilzzz.ecommerce.SB_Ecom.Repository;
 import com.nikhilzzz.ecommerce.SB_Ecom.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepo extends JpaRepository<Category,Integer> {
+public interface CategoryRepo extends JpaRepository<Category,Long> {
 }
